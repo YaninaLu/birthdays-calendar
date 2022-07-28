92d715872d23f05198770672639776b48e0237d8
Yanina Lubenska <yaninaosadchaya@gmail.com>
2022-07-28 22:51:49 +0300

    Added input list.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def main(users_dict: List[Dict[str, datetime]]) -> None:
     weekend is omitted).
     If run on Monday, the week starts with the previous Saturday and ends on Friday. If run on any other
     day the week starts with this day and ends in 7 days. People with birthdays on the weekend are put on Monday.
-
     :param users_dict: stores names of users as a string and birthdays as a datetime object
     :return: a list of lists where sublists store the names of people who have birthdays this week
     """
@@ -42,4 +41,15 @@ def main(users_dict: List[Dict[str, datetime]]) -> None:
 
 
 if __name__ == "__main__":
+    users = [{"name": "Mary", "birthday": datetime(1993, 7, 23)}, {"name": "Jim", "birthday": datetime(1994, 7, 24)},
+             {"name": "Sam", "birthday": datetime(1990, 7, 25)}, {"name": "Kate", "birthday": datetime(1988, 7, 26)},
+             {"name": "Olha", "birthday": datetime(1993, 7, 27)}, {"name": "Mary", "birthday": datetime(1999, 7, 28)},
+             {"name": "Volodymyr", "birthday": datetime(1990, 7, 29)}, {"name": "Mary", "birthday": datetime(1979, 7, 30)},
+             {"name": "Stasy", "birthday": datetime(1995, 7, 31)}, {"name": "Ivan", "birthday": datetime(1969, 8, 1)},
+             {"name": "Daria", "birthday": datetime(1990, 8, 2)}, {"name": "Timur", "birthday": datetime(1997, 8, 3)},
+             {"name": "Eugen", "birthday": datetime(1996, 8, 4)}, {"name": "Yana", "birthday": datetime(1993, 8, 5)},
+             {"name": "Andrew", "birthday": datetime(1991, 7, 28)}, {"name": "Serhiy", "birthday": datetime(1998, 7, 29)},
+             {"name": "Mattew", "birthday": datetime(1978, 7, 30)}, {"name": "Ksenia", "birthday": datetime(1995, 7, 31)},
+             {"name": "Trevor", "birthday": datetime(1989, 8, 1)}, {"name": "Vlad", "birthday": datetime(1993, 7, 29)}
+             ]
     main(users)
